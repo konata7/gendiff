3e001f66d16a18b01efc4c31fb5229d9c5ba6e4d
konata7 <nps777@live.ru>
2024-06-07 11:05:53 +0300

    refactor: split functions to reduce complexity

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,13 +1,15 @@
-const format = (diffObject) => Object.keys(diffObject).filter((key) => key.includes('__added')
+const filterUnchanged = (diffObject) => Object.keys(diffObject).filter((key) => key.includes('__added')
   || key.includes('__deleted')
-  || typeof diffObject[key] === 'object')
-  .map((key) => {
-    if (key.includes('__')) {
-      if (key.split('__')[1] === 'added') return `Property '${key.split('__')[0]}' was added with value: ${diffObject[key]}`;
-      if (key.split('__')[1] === 'deleted') return `Property '${key.split('__')[0]}' was removed`;
-    }
-    // eslint-disable-next-line no-underscore-dangle
-    return `Property '${key}' was updated. From ${diffObject[key].__old} to ${diffObject[key].__new}`;
-  }).join('\n');
+  || typeof diffObject[key] === 'object');
+
+const makeStringArray = (diffObject) => filterUnchanged(diffObject).map((key) => {
+  if (key.includes('__')) {
+    if (key.split('__')[1] === 'added') return `Property '${key.split('__')[0]}' was added with value: ${diffObject[key]}`;
+    if (key.split('__')[1] === 'deleted') return `Property '${key.split('__')[0]}' was removed`;
+  }
+  // eslint-disable-next-line no-underscore-dangle
+  return `Property '${key}' was updated. From ${diffObject[key].__old} to ${diffObject[key].__new}`;
+});
+const format = (diffObject) => makeStringArray(diffObject).join('\n');
 
 export default format;
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,4 +1,4 @@
-const format = (diffObject) => `{\n${Object.keys(diffObject).map((key) => {
+const makeStringArray = (diffObject) => Object.keys(diffObject).map((key) => {
   if (key.includes('__')) {
     if (key.split('__')[1] === 'added') return `  + ${key.split('__')[0]}: ${diffObject[key]}`;
     if (key.split('__')[1] === 'deleted') return `  - ${key.split('__')[0]}: ${diffObject[key]}`;
@@ -7,6 +7,7 @@ const format = (diffObject) => `{\n${Object.keys(diffObject).map((key) => {
   // eslint-disable-next-line no-underscore-dangle
   if (typeof diffObject[key] === 'object') return `  - ${key}: ${diffObject[key].__old}\n  + ${key}: ${diffObject[key].__new}`;
   return `    ${key}: ${diffObject[key]}`;
-}).join('\n')}\n}`;
+});
+const format = (diffObject) => `{\n${makeStringArray(diffObject).join('\n')}\n}`;
 
 export default format;
