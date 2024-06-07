cfd29a3d31c3e2e7f97cf79dfab7737ac46def37
konata7 <nps777@live.ru>
2024-06-07 10:41:38 +0300

    fix linter errors

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -4,6 +4,7 @@ const format = (diffObject) => `{\n${Object.keys(diffObject).map((key) => {
     if (key.split('__')[1] === 'deleted') return `  - ${key.split('__')[0]}: ${diffObject[key]}`;
   }
 
+  // eslint-disable-next-line no-underscore-dangle
   if (typeof diffObject[key] === 'object') return `  - ${key}: ${diffObject[key].__old}\n  + ${key}: ${diffObject[key].__new}`;
   return `    ${key}: ${diffObject[key]}`;
 }).join('\n')}\n}`;
