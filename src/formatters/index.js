98ccfa44e6ade7643b590a9140f4a9f915ad7179
konata7 <nps777@live.ru>
2024-06-10 17:10:33 +0300

    feat: add unsupported format handling

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,10 +2,18 @@ import stylishFormat from './stylish.js';
 import plainFormat from './plain.js';
 import jsonFormat from './json.js';
 
-const format = (diffObject, formatName = 'default') => {
-  if (formatName === 'plain') return plainFormat(diffObject);
-  if (formatName === 'json') return jsonFormat(diffObject);
-  return stylishFormat(diffObject);
+const mapping = {
+  json: (diffObject) => jsonFormat(diffObject),
+  plain: (diffObject) => plainFormat(diffObject),
+  stylish: (diffObject) => stylishFormat(diffObject),
+  unsupported: (formatName) => console.log(`Error: '${formatName}' output format is not supported`),
+};
+
+const format = (diffObject, formatName = 'stylish') => {
+  if (formatName === 'plain') return mapping.plain(diffObject);
+  if (formatName === 'json') return mapping.json(diffObject);
+  if (formatName === 'stylish') return mapping.stylish(diffObject);
+  return mapping.unsupported(formatName);
 };
 
 export default format;
