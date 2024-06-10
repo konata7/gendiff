821e6bf7c399254653b562aa75b092f101c3cf4e
konata7 <nps777@live.ru>
2024-06-10 17:13:04 +0300

    fix: works with file format instead of file extension

--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,10 @@
 import YAML from 'yaml';
 
 const parse = (data, format) => {
-  if (format === '.json') return JSON.parse(data);
-  if (format === '.yaml' || format === '.yml') return YAML.parse(data);
+  if (format === 'json') return JSON.parse(data);
+  if (format === 'yaml' || format === 'yml') return YAML.parse(data);
 
-  console.log(`${format} file extension is not supported.`);
+  console.log(`${format} file format is not supported.`);
 
   return 0;
 };
