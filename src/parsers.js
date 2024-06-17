4658932ef72f4ca33971299c946d65803250f690
konata7 <nps777@live.ru>
2024-06-17 18:10:03 +0300

    refactor: add mapping for file format types

--- src/parsers.js
+++ src/parsers.js
@@ -1,8 +1,12 @@
 import YAML from 'yaml';
 
+  const mapping = {
+    json: (data) => JSON.parse(data),
+    yaml: (data) => YAML.parse(data),
+    yml: (data) => YAML.parse(data),
+  }
 const parse = (data, format) => {
-  if (format === 'json') return JSON.parse(data);
-  if (format === 'yaml' || format === 'yml') return YAML.parse(data);
+  if (Object.hasOwn(mapping, format)) return mapping[format](data);
 
   console.log(`${format} file format is not supported.`);
 
